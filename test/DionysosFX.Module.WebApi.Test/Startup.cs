d41435ab01afb794fa180cf30b0915378c15f155
Ömer Faruk Şahin <ofaruksahin@outlook.com.tr>
2021-08-03 23:49:52 +0300

    Web Api Unit Test

---
+++ test/DionysosFX.Module.WebApi.Test/Startup.cs
@@ -0,0 +1,22 @@
+﻿using DionysosFX.Host;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DionysosFX.Module.WebApi.Test
+{
+    public class Startup : IStartup
+    {
+        public void Configure()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Build()
+        {
+            throw new NotImplementedException();
+        }        
+    }
+}

c13c56db5b125a8965c35940f579e176ef5b6cb1
Ömer Faruk Şahin <ofaruksahin@outlook.com.tr>
2021-08-24 18:44:00 +0300

    #5

--- src/DionysosFX.Module.OpenApi/OpenApiModule.cs
+++ src/DionysosFX.Module.OpenApi/OpenApiModule.cs
@@ -1,13 +1,7 @@
-﻿using DionysosFX.Module.WebApi;
-using DionysosFX.Swan.Modules;
+﻿using DionysosFX.Swan.Modules;
 using DionysosFX.Swan.Net;
-using System;
-using System.IO;
-using System.Linq;
-using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Xml;
 
 namespace DionysosFX.Module.OpenApi
 {
@@ -33,44 +27,7 @@ namespace DionysosFX.Module.OpenApi
 
         public void Start(CancellationToken cancellationToken)
         {
-            Assembly entryAssembly = Assembly.GetEntryAssembly();
-            var xmlPath = string.Format("{0}.xml", Path.Combine(Path.GetDirectoryName(entryAssembly.Location), entryAssembly.GetName().Name));
-            try
-            {
-                XmlDocument xmlDocument = new XmlDocument();
-                xmlDocument.Load(xmlPath);
-                XmlNodeList members = xmlDocument.SelectNodes("//doc//members//member");
-                foreach (XmlNode member in members)
-                {
-                    var memberName = member.Attributes["name"].Value;
-
-                    if (memberName.StartsWith("T"))
-                    {
-                        memberName = memberName.Replace("T:", "");
-                        var memberType = entryAssembly.GetType(memberName);
-                        if (memberType != null)
-                        {
-                            var isController = memberType.IsWebApiController();
-                            if (isController)
-                            {
-
-                            }
-                        }
-                    }
-                    else if (memberName.StartsWith("M:"))
-                    {
-                        memberName = memberName.Replace("M:", "");
-                        var memberNamePieces = memberName.Split('.').ToList();
-                        var namespaces = memberNamePieces.Where(f => !f.Contains("(") && !f.Contains(")")).ToList();
-                        string controllerName = string.Join(".", namespaces.Take(namespaces.Count - 1));
-                        var memberType = entryAssembly.GetType(controllerName);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+           
         }
     }
 }

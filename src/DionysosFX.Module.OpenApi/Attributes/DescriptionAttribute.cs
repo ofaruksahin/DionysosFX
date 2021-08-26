869f020aedecae6e5285ebcce545dda98f24b262
Ömer Faruk Şahin <ofaruksahin@outlook.com.tr>
2021-08-26 21:26:16 +0300

    #5

--- src/DionysosFX.Module.OpenApi/Attributes/DescriptionAttribute.cs
+++ src/DionysosFX.Module.OpenApi/Attributes/DescriptionAttribute.cs
@@ -2,6 +2,7 @@
 
 namespace DionysosFX.Module.OpenApi.Attributes
 {
+    [AttributeUsage(AttributeTargets.Class|AttributeTargets.Method,AllowMultiple =false)]
     public class DescriptionAttribute :Attribute
     {
         public string Description { get; set; }

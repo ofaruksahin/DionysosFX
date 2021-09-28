2960123b707cd5eea02c0771e2b8f8ae5269794a
Ömer Faruk Şahin <ofaruksahin@outlook.com.tr>
2021-09-28 20:48:54 +0300

    #34

--- src/DionysosFX.Extensibility/Properties/AssemblyInfo.cs
+++ src/DionysosFX.Extensibility/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DionysosFX.Module.ItemTemplate")]
+[assembly: AssemblyTitle("DionysosFX.Extensibility")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DionysosFX.Module.ItemTemplate")]
+[assembly: AssemblyProduct("DionysosFX.Extensibility")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
---
+++ template/item/DionysosFX.ItemTemplate.Controller/ControllerItem.cs
@@ -0,0 +1,14 @@
+﻿using DionysosFX.Module.WebApi;
+using DionysosFX.Swan.Routing;
+
+namespace $rootnamespace$.Controller
+{
+    [Route("/api/$safeitemname$")]
+    public class $safeitemname$ : WebApiController
+    {
+        public $safeitemname$()
+        {
+            
+        }
+    }
+}

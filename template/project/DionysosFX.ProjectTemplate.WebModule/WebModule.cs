86090d9c1a1fb80f374fe526e50f8b625a058cfd
Ömer Faruk Şahin <ofaruksahin@outlook.com.tr>
2021-10-06 20:46:51 +0300

    #33

---
+++ template/project/DionysosFX.ProjectTemplate.WebModule/WebModule.cs
@@ -0,0 +1,19 @@
+﻿using DionysosFX.Swan.Modules;
+using DionysosFX.Swan.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DionysosFX.ProjectTemplate.WebModule
+{
+    public class WebModule : WebModuleBase
+    {
+        public override void Start(CancellationToken cancellationToken)
+        {
+        }
+
+        public override async Task HandleRequestAsync(IHttpContext context)
+        {
+
+        }
+    }
+}
---
+++ template/project/DionysosFX.ProjectTemplate.WebModule/WebModuleExtension.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using DionysosFX.Swan;
+using DionysosFX.Swan.Exceptions;
+
+namespace DionysosFX.ProjectTemplate.WebModule
+{
+    public static class WebModuleExtension
+    {
+        public static IHostBuilder AddModule(this IHostBuilder @this)
+        {
+            @this
+                .ContainerBuilder
+                .RegisterType<WebModule>()
+                .SingleInstance();
+            return @this;
+        }
+
+        public static IHostBuilder UseModule(this IHostBuilder @this)
+        {
+            if (!@this.Container.TryResolve(out WebModule module))
+                throw new ModuleNotFoundException(typeof(WebModule).Name);
+            module.SetIContainer(@this.Container);
+            @this.ModuleCollection.Add(module.GetType().Name, module);
+            return @this;
+        }
+
+    }
+}
